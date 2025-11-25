0a6dbee857eb2d8b69f0315d104be816b356ab12
Ansh Gandhi <ansh.goodboy@gmail.com>
2025-11-25 04:19:42 -0800

    image parsing somewhat done, needs debug + testing

--- scrape_dataset/parse_images.py
+++ scrape_dataset/parse_images.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from PIL import Image
 from tqdm import tqdm
 
+from tangram import Piece, Tangram
+
 def extract_corners_from_image(image_path):
     NUM_COLORS = 7
     DEBUG = False
@@ -19,24 +21,37 @@ def extract_corners_from_image(image_path):
     colors = [np.array(color) for color, _ in color_counter.most_common(NUM_COLORS) if color[1] != 0]
 
     # get corners for tangram shape corresponding to each color
-    corners = {}
+    tangram = Tangram()
     for color in colors:
         # generate image mask
         mask = np.all(img == color, axis=-1).astype(np.uint8) * 255
 
-        # get contours around masked objects
+        # extract contours from mask
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        if len(contours) == 0:
-            continue
 
-        # choose largest contour
-        contour = contours[0]
+        # get largest contour
+        # TODO: debug/verify this logic
+        contour = None
+        for c in contours:
+            # TODO: remove duplicate points
+            if len(c) not in [3, 4]:
+                # print(len(c))
+                # breakpoint()
+                continue
+
+            contour = c
+            break
+
+        if contour is None:
+            continue
 
         # approximate polygon based on contour
         # epsilon is max distance between contour and approximate polygon, larger epsilon results in more simplified polygon
         epsilon = 0.02 * cv2.arcLength(contour, True)
-        approx_polygon = cv2.approxPolyDP(contour, epsilon, True)
-        corners[color] = [corner[0] for corner in approx_polygon]
+        corners = cv2.approxPolyDP(contour, epsilon, True).reshape(-1, 2)
+
+        # add piece to tangram
+        tangram.add_piece(Piece(corners, color))
 
         if DEBUG:
             # display mask
@@ -53,15 +68,32 @@ def extract_corners_from_image(image_path):
             # display image with marked corners
             corner_image = img.copy()
 
-            for corner in corners[color]:
+            for corner in corners:
                 cv2.circle(corner_image, tuple(corner), 4, (0, 255, 255), -1)
 
             cv2.imshow('Corner Image', cv2.cvtColor(corner_image, cv2.COLOR_HSV2BGR))
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
-    return corners
+        # add piece to tangram
+        tangram.add_piece(Piece(corners, color))
+
+    tangram.process(img.shape[1])
+
+    if DEBUG:
+        for piece in tangram.pieces:
+            center = (int(piece.pose[0]), int(piece.pose[1]))
+            cv2.circle(img, center, 4, (0, 255, 255), -1)
+
+            box = cv2.boxPoints((center, (32, 4), np.degrees(piece.pose[2]))).astype(np.int32)
+            cv2.fillPoly(img, [box], (0, 255, 255))
+
+        cv2.imshow('Output Image', cv2.cvtColor(img, cv2.COLOR_HSV2BGR))
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return tangram
 
 if __name__ == '__main__':
-    for image_path in tqdm((Path(__file__).parent / 'tangrams').iterdir()):
+    for image_path in tqdm(sorted(list((Path(__file__).parent / 'tangrams').iterdir()))):
         extract_corners_from_image(image_path)
---
+++ scrape_dataset/tangram.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+class Piece:
+    def __init__(self, coords, color):
+        # TODO: debug this part
+        if len(coords) not in [3, 4]:
+            print('something is wrong')
+            breakpoint()
+            return
+
+        self.coords = coords
+        self.color = color
+        self.shape = self.classify_shape()
+        self.area = self.calculate_area()
+        self.pose = None
+
+    def classify_shape(self):
+        if len(self.coords) == 3:
+            return 'triangle'
+
+        if np.dot(self.coords[0] - self.coords[1], self.coords[1] - self.coords[2]) == 0:
+            return 'square'
+
+        return 'parallelogram'
+
+    def calculate_area(self):
+        if 'triangle' in self.shape:
+            return 0.5 * np.linalg.det(np.hstack([self.coords, np.ones((3, 1))]))
+
+        if self.shape == 'square':
+            return np.linalg.norm(self.coords[0] - self.coords[1]) ** 2
+
+        return np.linalg.det(np.hstack([self.coords[:-1], np.ones((3, 1))]))
+
+    def reflect_image(self, image_y):
+        self.coords[:, 1] = image_y - self.coords[:, 1]
+
+    def calculate_pose(self):
+        theta = None
+        if 'triangle' in self.shape:
+            s2 = self.coords[0] - self.coords[1]
+            s0 = self.coords[2] - self.coords[1]
+            s2_mag = np.linalg.norm(s2)
+            s0_mag = np.linalg.norm(s0)
+
+            angle = np.arccos(np.dot(s2, s0) / (s2_mag * s0_mag))
+            if angle - 0.5 * np.pi < 0.05:
+                theta = np.arctan2(s2[1], s2[0])
+            elif s2_mag > s0_mag:
+                theta = np.arctan2(s0[1], s0[0])
+            else:
+                s1 = self.coords[2] - self.coords[0]
+                theta = np.arctan2(s1[1], s1[0])
+        elif self.shape == 'square':
+            base = self.coords[np.argsort(self.coords[:, 1])][:2]
+            base = base[1] - base[0]
+            theta = np.arctan2(base[1], base[0])
+        else:
+            # TODO: finish parallelogram heading implementation
+            sorted_arr = self.coords[np.lexsort((self.coords[:, 0], self.coords[:, 1]))][0]
+            theta = 0
+
+        return np.append(self.coords.mean(axis=0), theta)
+
+class Tangram:
+    def __init__(self, pieces=None, prompt=None):
+        self.pieces = pieces if pieces is not None else []
+        self.prompt = prompt if prompt is not None else ''
+
+    def add_piece(self, piece):
+        self.pieces.append(piece)
+
+    def paralellogram_wrong(self):
+        for piece in self.pieces:
+            if piece.shape == 'parallelogram':
+                s2 = piece.coords[0] - piece.coords[1]
+                s0 = piece.coords[2] - piece.coords[1]
+                s2_mag = np.linalg.norm(s2)
+                s0_mag = np.linalg.norm(s0)
+
+                angle = np.arccos(np.dot(s2, s0) / (s2_mag * s0_mag))
+                return (s2_mag > s0_mag) ^ (angle < 0.5 * np.pi)
+
+    def process(self, image_y):
+        # TODO: verify functionality of commented code
+        # sizes = ['small', 'small', 'medium', 'large', 'large']
+        # triangles = [(piece, i) for i, piece in enumerate(self.pieces) if 'triangle' in piece.shape]
+        # sorted_indices = np.argsort([triangle.area for triangle, _ in triangles])
+        # for idx, i in enumerate(sorted_indices):
+        #     self.pieces[triangles[i][1]].shape = f'{sizes[idx]} triangle'
+
+        # if self.paralellogram_wrong():
+        #     for piece in self.pieces:
+        #         piece.reflect_image(image_y)
+
+        for piece in self.pieces:
+            piece.pose = piece.calculate_pose()
